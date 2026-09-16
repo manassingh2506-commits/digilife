0d1eec815ea42b4b62cd35835ab93dece2afad30
Manas Singh <manassingh2506@gmail.com>
2026-09-16 16:31:38 +0530

    Add DigiLife app layout

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'DigiLife',description:'Minimal OLED document experience'};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
